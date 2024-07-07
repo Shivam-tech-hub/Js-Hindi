b4371fd446ee0d07866029c6368e2f9389bb524f
Shivam-tech-hub <itsmeshivam9116@gmail.com>
2024-07-07 05:59:03 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// primitive
+
+// 7 types : string, Number, Boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34565432544646465n
+
+
+
+// refrence  (non- primitive)
+
+// array, objects, functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "shivam",
+    age: 22,
+
+}
+ const myFunction = function(){
+    console.log("hello world");
+ }
+
+ console.log(typeof bigNumber);
+ console.log(typeof outsideTemp);
+ console.log(typeof scorevalue);
