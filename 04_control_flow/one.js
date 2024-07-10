d6d35ee906054b7de0eac44b1fcffc5df4632814
Shivam-tech-hub <itsmeshivam9116@gmail.com>
2024-07-10 06:00:18 +0000

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,46 @@
+// if 
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature < 50 ){
+   // console.log("less than 50");
+// }
+// console.log("temperature is greater than 50");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+
+// const score = 200
+
+// if (score > 100) {
+    // let power = "fly"
+   // console.log(`User power: ${power}`);
+// }
+
+//console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+   // console.log("less than 500");
+// } else if (balance < 750) {
+   // console.log("less than 750");
+
+// } else if (balance < 900) {
+   // console.log("less than 750");
+// } else {
+   // console.log("less than 1200");
+
+   // }
+
+
+   const UserLoggedIn = true
+   const debitCard = true
+
+   if (UserLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+   }
